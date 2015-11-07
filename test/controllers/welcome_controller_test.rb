361cd7ec10e6ef2074ce48004b5c9f9189e30235
geokira <gakirag@gmail.com>
2015-11-07 11:55:08 -0500

    Moved content from the template layout to the welcome:index layout and created a schools page under welcome

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -4,4 +4,10 @@ class WelcomeControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+    
+    test "should get home index" do
+        get :index
+        assert_response :success
+        assert_select "title", "Children Learning to Code in Orlando, FL | Code by Me"
+    end
 end
