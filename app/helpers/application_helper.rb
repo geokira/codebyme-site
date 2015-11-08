074dcaa5142aff46ba863c599f1a3014feab346d
geokira <gakirag@gmail.com>
2015-11-08 16:45:54 -0500

    Added integration test for the links in the layout of the page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+    
+    # Returns the full title on a per-page basis.
+    # If title is empty it returnsa default title for all pages. 
+    def full_title(page_title = '')
+        base_title = "Providing a Fun Learning environment for Young Coders and Creators in Orlando, FL"
+        if page_title.empty?
+            base_title
+        else
+            page_title + " | " + base_title
+        end
+    end
 end
