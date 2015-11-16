991992a058b18e3510969b583df2573e670d0d99
geo <gakirag@gmail.com>
2015-11-16 19:38:48 +0000

    added video to most pages. Still having to fix a few bugs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,20 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-/**
-*         <script src="startup-framework/common-files/js/jquery-1.10.2.min.js"></script>
-        <script src="startup-framework/flat-ui/js/bootstrap.min.js"></script>
-        <script src="startup-framework/common-files/js/jquery.scrollTo-1.4.3.1-min.js"></script>
-        <script src="startup-framework/common-files/js/modernizr.custom.js"></script>
-        <script src="startup-framework/common-files/js/page-transitions.js"></script>
-        <script src="startup-framework/common-files/js/easing.min.js"></script>
-        <script src="startup-framework/common-files/js/jquery.svg.js"></script>
-        <script src="startup-framework/common-files/js/jquery.svganim.js"></script>
-        <script src="startup-framework/common-files/js/jquery.parallax.min.js"></script>
-        <script src="startup-framework/common-files/js/startup-kit.js"></script>
-        <script src="startup-framework/common-files/js/jquery.backgroundvideo.min.js"></script>
-        <script src="startup-framework/common-files/js/jquery.parallax.min.js"></script>
-**/
+
 //= require jquery-1.10.2.min
 //= require bootstrap.min
 //= require jquery.scrollTo-1.4.3.1-min
