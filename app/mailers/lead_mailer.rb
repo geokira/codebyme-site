485aba4438adadcb33c0b6557fc915c88f5ce103
geo <gakirag@gmail.com>
2015-11-19 01:02:53 +0000

    Added Reply to Header into lead mailer and changed header 3 in homepage.

--- app/mailers/lead_mailer.rb
+++ app/mailers/lead_mailer.rb
@@ -1,23 +1,24 @@
 class LeadMailer < ApplicationMailer
-    default from: "dmiranda@codebyme.com"
-    
+    default from: "dmiranda@mg.codebyme.com"
+
     messsage={}
-    
+
     def welcome_to_new_lead_email(lead)
         @lead = lead
         message={}
-        message[:from] = "Dorian @ Code by Me <dmiranda@codebyme.com>"
+        message[:from] = "Dorian @ Code by Me <dmiranda@mg.codebyme.com>"
         message[:to] = @lead.email
         message[:subject] = "Code by Me say Thank You!"
-        mail(message)
+        email = mail(message)
+        email.mailgun_headers = {"Reply-To": 'info@codebyme.com'}
     end
-    
+
     def notify_about_new_lead(lead)
         @lead = lead
         message={}
-        message[:from] = "New Lead Code by Me <noreply@codebyme.com>"
+        message[:from] = "New Lead Code by Me <noreply@mg.codebyme.com>"
         message[:to] = "info@codebyme.com"
-        message[:subject] = Time.zone.now.strftime("%Y%m%d-%^b-%j-%l:%M %p") + " New Lead: " + @lead.name 
+        message[:subject] = Time.zone.now.strftime("%Y%m%d-%^b-%j-%l:%M %p") + " New Lead: " + @lead.name
         mail(message)
     end
 end
